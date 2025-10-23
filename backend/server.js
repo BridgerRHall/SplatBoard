9b0e2f2b03a682c503bb257a2e28c98d16d79bbf
Bridger Hall <bridgerrhall@gmail.com>
2025-10-22 18:36:56 -0600

    added req and res and tested with postman

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,7 @@ const app = express();
 app.listen(5000, () => {
   console.log("this is the start of the splat board backend");
 });
+
+app.get("/api/splats", (req, res) => {
+  res.send("This is the first res of the splatboard app");
+});
