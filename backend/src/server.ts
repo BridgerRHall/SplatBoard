2f4b830201e7313bcf61a1d3a41035e9c149a984
Bridger Hall <bridgerrhall@gmail.com>
2025-10-29 18:32:25 -0600

    put port in .env file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import router from "./routes/splatRoutes.js";
 import connectDB from "./config/db.js";
+import * as dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+const port = process.env.PORT;
 
 connectDB();
 app.use("/api/splats", router);
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("this is the start of the splat board backend");
 });
