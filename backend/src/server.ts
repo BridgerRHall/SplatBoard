d70516a50b74c385d7cc3dac0ff232eb515b9227
Bridger Hall <bridgerrhall@gmail.com>
2025-11-03 22:24:36 -0700

    setup front end and react router

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,8 +9,6 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-connectDB();
-
 //middleware
 //get access to req i.e. parse json bodyies
 app.use(express.json());
@@ -26,7 +24,9 @@ app.use((req, res, next) => {
 //direct api calls based on req body
 app.use("/api/splats", router);
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-  console.log("this is the start of the splat board backend");
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+    console.log("this is the start of the splat board backend");
+  });
 });
